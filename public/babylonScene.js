443bdc1478f14ea881707fbe051d7df6c5c86bb3
Anonymous <rishusingal568@gmail.com>
2022-09-01 00:32:37 +0530

    Added model in BabylonJS

--- public/babylonScene.js
+++ public/babylonScene.js
@@ -1,28 +1,23 @@
 var canvas = document.getElementById("renderCanvas");
 var createScene = function () {
-    var scene = new BABYLON.Scene(engine);
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-		camera.setTarget(BABYLON.Vector3.Zero());
-		camera.attachControl(canvas, true);
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-		light.intensity = 0.7;
-    var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-        
-	var url;
-	var fileName;
 	
-//-- BoomBox.gltf
-	url = "https://raw.githubusercontent.com/BabylonJS/Babylon.js/master/Playground/scenes/BoomBox/";
-	fileName = "BoomBox.gltf";
+	var scene = new BABYLON.Scene(engine);
 
-	BABYLON.SceneLoader.ImportMesh("", url, fileName, scene, function (newMeshes) {
-		var mesh = newMeshes[0];
-			mesh.position.copyFromFloats(0, 1, 0);
-			mesh.scaling.copyFromFloats(100,100,100);
-			
-		camera.target = mesh;
-		console.log(mesh)
+	var camera = new BABYLON.ArcRotateCamera("camera",Math.PI/2,Math.PI/2,5,new BABYLON.Vector3(0,0,0));
+	camera.attachControl(canvas,true);
+
+	// var box = new BABYLON.MeshBuilder.CreateBox("box");
+
+	var light = new BABYLON.HemisphericLight("light",new BABYLON.Vector3(1,1,0));
+	
+	// ONLINE HOSTED MODELS
+	BABYLON.SceneLoader.Append("https://assets.babylonjs.com/meshes/","both_houses_scene.babylon",scene);
+
+	// LOCAL MODELS
+	BABYLON.SceneLoader.Append("./","cabinet.glb",scene,function(){
+
 	});
+
     return scene;
 };
 
